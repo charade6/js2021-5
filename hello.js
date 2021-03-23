864c69161ff8b60f4fb77b77fcecd291017adb5e
charade6 <charade6@naver.com>
2021-03-23 19:54:18 +0900

    4주차수업 커밋

--- hello.js
+++ hello.js
@@ -1,3 +1,8 @@
-console.log("hello world...!");
-console.log("hello world...!");
-console.log("hello world...!");
+let date = new Date();
+h = date.getHours();
+
+console.log(h < 3 || h > 8);
+console.log(h <= 3 && h <= 8);
+
+let type = typeof(date.getHours());
+console.log(type);
